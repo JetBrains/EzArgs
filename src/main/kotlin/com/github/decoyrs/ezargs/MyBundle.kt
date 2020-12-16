d3d4c1783a0f5692bf43674904b9e0cbce2c065b
GitHub Action <action@github.com>
2020-12-16 09:41:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/decoyrs/ezargs/MyBundle.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.decoyrs.ezargs
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/decoyrs/ezargs/services/MyProjectService.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.decoyrs.ezargs.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.decoyrs.ezargs.MyBundle
 
 class MyProjectService(project: Project) {
 
