3e3719873847253142e2b1c51eec57c28e49ac58
Alexander Pirogov <alexander.pirogov@jetbrains.com>
2021-03-09 13:04:33 +0300

    fixes for CR

--- src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
@@ -2,49 +2,34 @@ package com.github.decoyrs.ezargs.actions
 
 import com.github.decoyrs.ezargs.EzArgsBundle
 import com.github.decoyrs.ezargs.services.EzArgsService
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
-import com.intellij.openapi.editor.event.DocumentEvent
-import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.project.Project
 import com.intellij.ui.EditorComboBox
+import com.jetbrains.rd.platform.util.lifetime
+import com.jetbrains.rd.platform.util.project
+import javax.swing.JComponent
 
-class WrappedEditorComboBox(var isInitialized: Boolean = false) :
-    EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip"))
-
-class ArgumentsInputField : AnAction(), CustomComponentAction, DocumentListener {
-    private var project: Project? = null
-
-    override fun createCustomComponent(presentation: Presentation, place: String) = WrappedEditorComboBox()
-
-    override fun update(e: AnActionEvent) {
-        val editorComboBox = e.presentation.getClientProperty(CustomComponentAction.COMPONENT_KEY)
-        if (editorComboBox !is WrappedEditorComboBox) return
-        if (editorComboBox.isInitialized) return
-        project = e.project ?: return
-
-        editorComboBox.addDocumentListener(this)
-        val service = project!!.getService(EzArgsService::class.java)
-        editorComboBox.setHistory(service.history.toTypedArray())
-        editorComboBox.isInitialized = true
-        service.addHistoryListener {
-            editorComboBox.setHistory(it.toTypedArray())
-            val lastElement = editorComboBox.model.getElementAt(editorComboBox.model.size - 1)
-            editorComboBox.model.selectedItem = lastElement
+class ArgumentsInputField : AnAction(), CustomComponentAction {
+    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext): JComponent {
+        return EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip")).apply {
+            val project = context.project!!
+            val service = EzArgsService.getInstance(project)
+            setHistory(service.history.toTypedArray())
+            service.addHistoryListener(project.lifetime) { history ->
+                setHistory(history.toTypedArray())
+                val lastElement = model.getElementAt(model.size - 1)
+                model.selectedItem = lastElement
+            }
+            project.lifetime.bracket(
+                    { addDocumentListener(service) },
+                    { removeDocumentListener(service) }
+            )
         }
     }
 
     override fun actionPerformed(p0: AnActionEvent) {
-        TODO("Not yet implemented")
-    }
-
-    override fun documentChanged(event: DocumentEvent) {
-        super.documentChanged(event)
-        if (project == null) return
-
-        val service = project!!.getService(EzArgsService::class.java)
-        service.arguments = event.document.text
+        if (p0.place == ActionPlaces.KEYBOARD_SHORTCUT) {
+            // TODO: focus actions OR show mini text editor with run button
+        }
     }
 }
--- src/main/kotlin/com/github/decoyrs/ezargs/services/EzArgsService.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/services/EzArgsService.kt
@@ -2,34 +2,42 @@ package com.github.decoyrs.ezargs.services
 
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.project.Project
+import com.jetbrains.rd.platform.util.application
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.intellij.openapi.editor.event.DocumentListener
 
-inline fun <T> kotlin.collections.List<T>.lastOrElse(defaultValue: () -> T): T {
-    return try {
-        last()
-    } catch (e: NoSuchElementException) {
-        defaultValue()
-    }
-}
+inline fun <T> kotlin.collections.List<T>.lastOrElse(defaultValue: () -> T): T  = lastOrNull() ?: defaultValue()
 
 fun interface HistoryListener {
     fun invoke(history: List<String>)
 }
 
 @Service
-class EzArgsService(private val project: Project) {
+class EzArgsService(private val project: Project) : DocumentListener {
     companion object {
         const val ARGUMENTS_HISTORY_PROPERTY = "ezargs.argumentsList"
+        fun getInstance(project: Project): EzArgsService  = project.service()
     }
 
     private val historyListeners = mutableListOf<HistoryListener>()
-    fun addHistoryListener(listener: HistoryListener) = historyListeners.add(listener)
+    fun addHistoryListener(lifetime: Lifetime, listener: HistoryListener) {
+        application.assertIsDispatchThread()
+        lifetime.bracket(
+                { historyListeners.add(listener) },
+                { historyListeners.remove(listener) }
+        )
+    }
 
     val history = PropertiesComponent.getInstance(project).getValues(ARGUMENTS_HISTORY_PROPERTY)?.toMutableList()
         ?: mutableListOf<String>()
     var arguments = history.lastOrElse { "" }
 
     fun addToHistory(newArguments: String) {
+        application.assertIsDispatchThread()
+
         if (history.contains(newArguments)) return
 
         history.add(newArguments)
@@ -37,5 +45,10 @@ class EzArgsService(private val project: Project) {
         historyListeners.forEach {
             it.invoke(history)
         }
+    }
+
+    override fun documentChanged(event: DocumentEvent) {
+        super.documentChanged(event)
+        arguments = event.document.text
     }
 }
