963211b3406b8c0d42b5296c5029ddd4548d0466
Alexander Pirogov <alexander.pirogov@jetbrains.com>
2021-03-09 17:30:55 +0300

    Rewrite action back to initialization in update due to multiple instances of component being created

--- src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
@@ -2,29 +2,38 @@ package com.github.decoyrs.ezargs.actions
 
 import com.github.decoyrs.ezargs.EzArgsBundle
 import com.github.decoyrs.ezargs.services.EzArgsService
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.ui.EditorComboBox
 import com.jetbrains.rd.platform.util.lifetime
-import com.jetbrains.rd.platform.util.project
-import javax.swing.JComponent
+
+class WrappedEditorComboBox(var isInitialized:Boolean = false):EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip"))
+
 
 class ArgumentsInputField : AnAction(), DumbAware, CustomComponentAction {
-    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext): JComponent {
-        return EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip")).apply {
-            val project = context.project ?: return@apply
-            val service = EzArgsService.getInstance(project)
-            setHistory(service.history.toTypedArray())
-            service.addHistoryListener(project.lifetime) { history ->
-                setHistory(history.toTypedArray())
-                val lastElement = model.getElementAt(model.size - 1)
-                model.selectedItem = lastElement
-            }
-            project.lifetime.bracket(
-                    { addDocumentListener(service) },
-                    { removeDocumentListener(service) }
-            )
+    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext) = WrappedEditorComboBox()
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        val editorComboBox = e.presentation.getClientProperty(CustomComponentAction.COMPONENT_KEY)
+        if (editorComboBox !is WrappedEditorComboBox) return
+        if (editorComboBox.isInitialized) return
+        val project = e.project ?: return
+
+        editorComboBox.isInitialized = true
+        val service = EzArgsService.getInstance(project)
+        project.lifetime.bracket(
+                {editorComboBox.addDocumentListener(service)                },
+                {editorComboBox.removeDocumentListener(service)}
+        )
+        editorComboBox.setHistory(service.history.toTypedArray())
+        service.addHistoryListener(project.lifetime) {
+            editorComboBox.setHistory(it.toTypedArray())
         }
     }
 
