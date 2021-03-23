e4509af5ad43da195b6b6aa5e591f92c3bfc2df1
Alexander Pirogov <alexander.pirogov@jetbrains.com>
2021-03-23 16:19:51 +0300

    Fix mem leak on closing IDE * EditorComboBox can't be destroyed properly without project passed to constructor

--- src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
+++ src/main/kotlin/com/github/decoyrs/ezargs/actions/ArgumentsInputField.kt
@@ -8,33 +8,50 @@ import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction
+import com.intellij.openapi.fileTypes.FileTypes
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
 import com.intellij.ui.EditorComboBox
 import com.jetbrains.rd.platform.util.lifetime
+import com.jetbrains.rd.platform.util.project
+import com.jetbrains.rd.util.lifetime.onTermination
+import javax.swing.JPanel
 
-class WrappedEditorComboBox(var isInitialized:Boolean = false):EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip"))
-
+class WrappedEditorComboBox(var isInitialized:Boolean = false):JPanel()
 
 class ArgumentsInputField : AnAction(), DumbAware, CustomComponentAction {
-    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext) = WrappedEditorComboBox()
+    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext) = WrappedEditorComboBox().apply {
+        if(context.project == null) return@apply
+
+        initComponent(context.project!!, this)
+    }
 
     override fun update(e: AnActionEvent) {
         super.update(e)
-        val editorComboBox = e.presentation.getClientProperty(CustomComponentAction.COMPONENT_KEY)
-        if (editorComboBox !is WrappedEditorComboBox) return
-        if (editorComboBox.isInitialized) return
+        val wrappedEditorComboBox = e.presentation.getClientProperty(CustomComponentAction.COMPONENT_KEY)
+        if (wrappedEditorComboBox !is WrappedEditorComboBox) return
+        if (wrappedEditorComboBox.isInitialized) return
         val project = e.project ?: return
 
-        editorComboBox.isInitialized = true
+        initComponent(project, wrappedEditorComboBox)
+    }
+
+    private fun initComponent(project: Project, wrappedEditorComboBox:WrappedEditorComboBox) {
+        wrappedEditorComboBox.isInitialized = true
+        val editorComboBox = EditorComboBox(EzArgsBundle.message("action.EzArgs.ArgumentsInputFieldAction.tooltip"), project, FileTypes.PLAIN_TEXT)
+        project.lifetime.onTermination {
+            wrappedEditorComboBox.parent?.remove(wrappedEditorComboBox)
+        }
         val service = EzArgsService.getInstance(project)
         project.lifetime.bracket(
-                {editorComboBox.addDocumentListener(service)                },
+                {editorComboBox.addDocumentListener(service)},
                 {editorComboBox.removeDocumentListener(service)}
         )
         editorComboBox.setHistory(service.history.toTypedArray())
         service.addHistoryListener(project.lifetime) {
             editorComboBox.setHistory(it.toTypedArray())
         }
+        wrappedEditorComboBox.add(editorComboBox)
     }
 
     override fun actionPerformed(p0: AnActionEvent) {
