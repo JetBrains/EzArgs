0d2f55b9170a25c2e41630f80785a6b43dd34896
Alexander Pirogov <a.g.pirogov@gmail.com>
2021-08-11 12:27:27 +0300

    Set fixed size for edit field TODO: add button to open a big edit zone

--- src/main/kotlin/com/jetbrains/rider/ezargs/actions/ArgumentsInputField.kt
+++ src/main/kotlin/com/jetbrains/rider/ezargs/actions/ArgumentsInputField.kt
@@ -6,69 +6,85 @@ import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.IdeFrame
 import com.intellij.ui.EditorComboBox
 import com.intellij.ui.LanguageTextField
+import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.util.textCompletion.TextCompletionUtil
+import com.intellij.util.ui.JBUI
 import com.jetbrains.rd.platform.util.lifetime
-import com.jetbrains.rd.platform.util.project
-import com.jetbrains.rd.util.lifetime.onTermination
 import com.jetbrains.rider.ezargs.completion.CmdArgsCompletionProvider
 import com.jetbrains.rider.ezargs.services.EzArgsService
+import java.awt.Dimension
 import javax.swing.JComponent
 import javax.swing.JPanel
-
-class WrappedEditorComboBox(var isInitialized: Boolean = false) : JPanel()
+import javax.swing.SwingUtilities
 
 class ArgumentsInputField : AnAction(), DumbAware, CustomComponentAction {
-    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext): JComponent {
-        return WrappedEditorComboBox().apply {
-            if (context.project == null) return@apply
-
-            initComponent(context.project!!, this)
+    override fun actionPerformed(p0: AnActionEvent) {
+        if (p0.place == ActionPlaces.KEYBOARD_SHORTCUT) {
+            // Good first task: focus actions OR show mini text editor with run button
         }
     }
 
     override fun update(e: AnActionEvent) {
-        super.update(e)
-        val wrappedEditorComboBox = e.presentation.getClientProperty(CustomComponentAction.COMPONENT_KEY)
-        if (wrappedEditorComboBox !is WrappedEditorComboBox) return
-        if (wrappedEditorComboBox.isInitialized) return
-        val project = e.project ?: return
-
-        initComponent(project, wrappedEditorComboBox)
+        e.presentation.isEnabledAndVisible = true
     }
 
-    private fun initComponent(project: Project, wrappedEditorComboBox: WrappedEditorComboBox) {
-        wrappedEditorComboBox.isInitialized = true
-        val service = EzArgsService.getInstance(project)
-        val documentCreator = TextCompletionUtil.DocumentWithCompletionCreator(CmdArgsCompletionProvider, true)
-        val document = LanguageTextField.createDocument(
-            service.arguments, PlainTextLanguage.INSTANCE, project,
-            documentCreator
-        )
-        val editorComboBox = EditorComboBox(
-            document,
-            project,
-            PlainTextFileType.INSTANCE
-        )
-        project.lifetime.onTermination {
-            wrappedEditorComboBox.parent?.remove(wrappedEditorComboBox)
-        }
-        project.lifetime.bracket(
-            { editorComboBox.addDocumentListener(service) },
-            { editorComboBox.removeDocumentListener(service) }
-        )
-        editorComboBox.setHistory(service.history.toTypedArray())
-        service.addHistoryListener(project.lifetime) {
-            editorComboBox.setHistory(it.toTypedArray())
-        }
-        editorComboBox.selectedItem = service.arguments
-        wrappedEditorComboBox.add(editorComboBox)
-    }
+    override fun createCustomComponent(presentation: Presentation, place: String, context: DataContext): JComponent {
+        return object : JPanel(VerticalLayout(JBUI.scale(2))) {
+            private var editorComboBox: EditorComboBox? = null
+            private var project: Project? = null
+                set(value) {
+                    if(field == value) return
+                    destroy()
+                    field = value
+                    value?.let{
+                        createAndAdd(it)
+                    }
+                }
+            override fun addNotify() {
+                super.addNotify()
+                (SwingUtilities.getWindowAncestor(this) as? IdeFrame)?.project?.let {
+                    project = it
+                }
+            }
+            private fun destroy() {
+                editorComboBox?.let {
+                    if(components.contains(it)) {
+                        remove(it)
+                    }
+                }
+            }
+            private fun createAndAdd(project: Project) {
+                val service = EzArgsService.getInstance(project)
+                val documentCreator = TextCompletionUtil.DocumentWithCompletionCreator(CmdArgsCompletionProvider, true)
+                val document = LanguageTextField.createDocument(
+                    service.arguments,
+                    PlainTextLanguage.INSTANCE,
+                    project,
+                    documentCreator
+                )
+                val newComboBox = object : EditorComboBox(
+                    document,
+                    project,
+                    PlainTextFileType.INSTANCE
+                ) {
+                    override fun getPreferredSize() = Dimension(JBUI.scale(200), super.getPreferredSize().height)
+                }
 
-    override fun actionPerformed(p0: AnActionEvent) {
-        if (p0.place == ActionPlaces.KEYBOARD_SHORTCUT) {
-            // Good first task: focus actions OR show mini text editor with run button
+                project.lifetime.bracket(
+                    { newComboBox.addDocumentListener(service) },
+                    { newComboBox.removeDocumentListener(service) }
+                )
+                newComboBox.setHistory(service.history.toTypedArray())
+                service.addHistoryListener(project.lifetime) {
+                    newComboBox.setHistory(it.toTypedArray())
+                }
+                newComboBox.selectedItem = service.arguments
+                editorComboBox = newComboBox
+                add(editorComboBox)
+            }
         }
     }
 }
